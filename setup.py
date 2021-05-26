4b2244139b771acafa4a09c0bbf20ca5342e186b
zaa <80369423+zaafirrahman@users.noreply.github.com>
2021-05-26 20:13:05 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+def readme() -> str:
+    with open(r'README.md') as f:
+        README = f.read()
+    return README
+
 classifiers = [
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Education',
     'Intended Audience :: Financial and Insurance Industry',
     'Operating System :: OS Independent',
-    'License :: OSI Approved :: MIT Lisence',
+    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3'
 ]
 
@@ -13,8 +18,9 @@ setup(
     name='compoundinterest',
     version='0.0.1',
     description='Module of compound interest factors which includes single payment, uniform payment series, and arithmetic gradient calculations',
-    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-    url=''
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/zaafirrahman/compoundinterest'
     author='Zaafirrahman',
     author_email='zaafir123rahman@gmail.com',
     license='MIT',
